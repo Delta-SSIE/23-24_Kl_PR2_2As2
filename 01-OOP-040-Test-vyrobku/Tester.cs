89092ba848fb9028b32f0f86e7b3a4cfd571550f
Jan Koupil <jan.koupil@gmail.com>
2023-10-10 10:14:20 +0200

    2023-10-03 OOP

---
+++ 01-OOP-040-Test-vyrobku/Tester.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_040_Test_vyrobku
+{
+    internal class Tester
+    {
+		private Vyrobek _vzor;
+
+		public Vyrobek Vzor
+		{
+			get { return _vzor; }
+			set {
+				if (value == null)
+                    throw new ArgumentNullException();
+
+				_vzor = value; 
+			}
+		}
+
+		private double _tolerance;
+
+		/// <summary>
+		/// Označuje toleranci v procentech
+		/// </summary>
+		public double Tolerance
+		{
+			get { return _tolerance; }
+			set {
+				if (value <= 0)
+					throw new ArgumentOutOfRangeException();
+
+				_tolerance = value; 
+			}
+		}
+        /// <summary>
+        /// Vytvoří novou testovácí třídu
+        /// </summary>
+        /// <param name="vzor">Vzorový výrobek, se kterým se bude porovnávat</param>
+        /// <param name="tolerance">Tolerance v procentech</param>
+        public Tester(Vyrobek vzor, double tolerance)
+        {
+            Vzor = vzor;
+            Tolerance = tolerance;
+        }
+
+		public bool Vyhovuje(Vyrobek vyrobek)
+		{
+			// o kolik se liší v procentech
+			double odchylka = (vyrobek.Rozmer - Vzor.Rozmer) / Vzor.Rozmer * 100;
+			
+			//odpovím porovnáním s tolerancí
+			if (Math.Abs(odchylka) > Tolerance)
+				return false;
+			else
+				return true;
+		}
+    }
+}
---
+++ 01-OOP-040-Test-vyrobku/Vyrobek.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_OOP_040_Test_vyrobku
+{
+    internal class Vyrobek
+    {
+		private double _rozmer;
+
+		public double Rozmer
+		{
+			get { return _rozmer; }
+			set 
+			{
+				if (value <= 0)
+					throw new ArgumentOutOfRangeException();
+
+				_rozmer = value; 
+			}
+		}
+
+        public Vyrobek(double rozmer)
+        {
+            Rozmer = rozmer;
+        }
+    }
+}
