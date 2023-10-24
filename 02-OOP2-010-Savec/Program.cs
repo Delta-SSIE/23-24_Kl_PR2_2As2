dc8dea7ff7f4bfe20a5cbd476a5ebe800f12c2ab
Jan Koupil <jan.koupil@gmail.com>
2023-10-24 11:55:47 +0200

    2023-10-24 OOP: dědičnost

---
+++ 02-OOP2-010-Savec/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _02_OOP2_010_Savec
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kun ferda = new Kun();
+            ferda.Cvalej();
+            ferda.Dychej();
+            ferda.SajMleko();
+
+            Velryba mobyDick = new Velryba();
+            mobyDick.Plav();
+            mobyDick.Dychej();
+            mobyDick.SajMleko();
+
+            //mobyDick.Cvalej();
+
+            Savec konik = new Kun(); //lze, kun je savec
+            //konik.Cvalej(); //nechce, protože třída Savec nemá metodu cvalej
+
+            Kun kopieKone = (Kun)konik;
+            kopieKone.Cvalej();
+
+            Savec kopieVelryby = mobyDick;
+            // Kun krizenec = (Kun)kopieVelryby; //nelze uvnitř je velryba, velryba není kůň
+            // krizenec.Cvalej();
+        }
+    }
+
+    class Savec //bázová třída ... to společné
+    {
+        public void Dychej()
+        {
+            Console.WriteLine("Nádech - výdech");
+        }
+        public void SajMleko()
+        {
+            Console.WriteLine("... ... ...");
+        }
+    }
+
+    class Kun : Savec //odvozená třída ... to, čím se liší od bázové
+    {
+        public void Cvalej()
+        {
+            Console.WriteLine("Kopyta pádí");
+        }
+    }
+
+    class Velryba : Savec
+    {
+        public void Plav()
+        {
+            Console.WriteLine("Šplouch");
+        }
+    }
+}
---
+++ 02-OOP2-020-Konstruktor-bazove/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace _02_OOP2_020_Konstruktor_bazove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Kun ferda = new Kun("Ferda");
+            ferda.Cvalej();
+            ferda.Dychej();
+            ferda.SajMleko();
+            //ferda.Jmeno = "Karel"; //nelze - je chráněno
+        }
+    }
+    class Savec //bázová třída ... to společné
+    {
+        protected string Jmeno { get; set; } //i dědící třídy mohou vidět/měnit
+        public Savec(string jmeno)
+        {
+            Jmeno = jmeno;
+        }
+        public void Dychej()
+        {
+            Console.WriteLine("Nádech - výdech");
+        }
+        public void SajMleko()
+        {
+            Console.WriteLine("... ... ...");
+        }
+    }
+
+    class Kun : Savec //odvozená třída ... to, čím se liší od bázové
+    {
+        public Kun(string jmeno) : base(jmeno)
+        {
+        }
+        public void Cvalej()
+        {
+            Console.WriteLine("Kopyta pádí");
+        }
+        public void PredstavSe()
+        {
+            Console.WriteLine($"Já jsem {Jmeno}, íhahá"); 
+            //jméno je dostupné, i když kód není přímo ve třídě savec.
+            //Protected připouští i dědící třídu
+        }
+    }
+
+    class Velryba : Savec
+    {
+        public Velryba(string jmeno) : base(jmeno)
+        {
+        }
+
+        public void Plav()
+        {
+            Console.WriteLine("Šplouch");
+        }
+    }
+}
