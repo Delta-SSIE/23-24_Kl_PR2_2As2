760a72b23dcd18c440aa7567121af553f34e2f0b
Jan Koupil <jan.koupil@gmail.com>
2024-04-02 10:26:15 +0200

    2025-04-02 DB - SQL a EF

---
+++ 05-DB-02-EntityFrameworkSimple/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DB_EF_Simple
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //vložím do db
+            using (var dbContext = new MyDbContext())
+            {
+                Car newCar;
+
+                newCar = new Car { RegPlate = "1E15871", Brand = "Toyota", Model = "Corolla", Purchased = DateTime.Now };
+                dbContext.Cars.Add(newCar);
+                dbContext.SaveChanges();
+
+                newCar = new Car { RegPlate = "4L29114", Brand = "Volkswagen", Model = "Golf", Purchased = DateTime.Now };
+                dbContext.Cars.Add(newCar);
+                dbContext.SaveChanges();
+            }
+
+
+            //vylistuju
+            using (var dbContext = new MyDbContext())
+            {
+                var cars = dbContext.Cars.ToList();
+                foreach (var car in cars)
+                    Console.WriteLine($"{car.Brand} {car.Model} ({car.RegPlate})");
+
+                Console.WriteLine();
+            }
+
+            //vyberu konkrétní
+            using (var dbContext = new MyDbContext())
+            {
+                var car = dbContext.Cars.FirstOrDefault(c => c.Brand == "Toyota");
+                Console.WriteLine($"{car.Brand} {car.Model} ({car.RegPlate})"); //vypíšu ho
+                Console.WriteLine();
+
+                car.Model = "Yaris";
+                dbContext.SaveChanges();
+            }
+
+            //vylistuju
+            using (var dbContext = new MyDbContext())
+            {
+                var cars = dbContext.Cars.ToList();
+                foreach (var car in cars)
+                    Console.WriteLine($"{car.Brand} {car.Model} ({car.RegPlate})");
+
+                Console.WriteLine();
+            }
+
+            //uklidím po sobě
+            using (var dbContext = new MyDbContext())
+            {
+                var allCars = dbContext.Cars.ToList();
+
+                foreach (var car in allCars)
+                {
+                    dbContext.Cars.Remove(car);
+                }
+
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
