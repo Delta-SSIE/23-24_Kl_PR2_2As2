c2106c9790c6c60e041085dd518d81f551e1ab5e
Jan Koupil <jan.koupil@gmail.com>
2024-04-16 12:02:34 +0200

    2024-04-16 Calculator zaklad

---
+++ 06-WPF-08-Calculator/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _06_WPF_08_Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        enum Operation { Add, Subtract, Multiply, Divide }
+
+        private double _lastNumber;
+        private Operation _operation;
+        private string _currentText;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RenderDisplay()
+        {
+            DisplayTB.Text = _currentText;
+        }
+
+        private void numberBtnClick(object sender, RoutedEventArgs e)
+        {
+            Button pressedBtn = (Button)sender;
+            string pressedNum = pressedBtn.Content.ToString();
+
+            if (pressedNum == "0" && _currentText == "0")
+                return;
+
+            _currentText += pressedNum;
+            RenderDisplay();
+        }
+
+        private void ACBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _currentText = "0";
+            RenderDisplay();
+        }
+
+        private void percentBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ 06-WPF-08-Calculator/MyMath.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06_WPF_08_Calculator
+{
+    internal static class MyMath
+    {
+        public static double Add(double num1, double num2) => num1 + num2;
+        public static double Subtract(double num1, double num2) => num1 - num2;
+        public static double Multiply(double num1, double num2) => num1 * num2;
+        public static double Divide(double num1, double num2) => num1 / num2;
+        public static double Percent(double num) => num / 100;         
+    }
+}
